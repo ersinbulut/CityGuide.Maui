00006689a65e26235c09bd31fcdc8efdf6a4c895
BULUTS <BULUTS@BULUTS>
2026-07-10 11:22:08 +0300

    Add project files.

---
+++ CityGuide.Maui/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace CityGuide.Maui
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
